07feb218536d53e7e88e5ada22d0e812d73e9203
hsnyc <henry@hsnyc.co>
2016-11-01 23:34:09 -0400

    Added smooth scroll script
    Enqueued in functions.php file.

--- functions.php
+++ functions.php
@@ -3,4 +3,10 @@
  * BoneJoint functions and definitions
  */
 
+ function bonejoint_scripts() {
+	wp_enqueue_script( 'jquery3', get_stylesheet_directory_uri() . '/js/jquery.min.js', array(), '3.1.1', true );
+    wp_enqueue_script( 'scroll', get_stylesheet_directory_uri() . '/js/scroll.js', array(), '1.0.0', true );
+}
+add_action( 'wp_enqueue_scripts', 'bonejoint_scripts' );
+
 ?>
